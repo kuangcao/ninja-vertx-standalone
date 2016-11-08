c6b214fa0bf3d461b0313f193344487b21ccffd2
huwei <huwei@eking.mobi>
2016-11-08 10:44:47 +0800

    调整验证的代码到单独的Handler里面， SockJSHandler不需要做变动。

--- ninja-vertx-sample/src/main/java/conf/VertxRoutes.java
+++ ninja-vertx-sample/src/main/java/conf/VertxRoutes.java
@@ -5,27 +5,30 @@ import com.google.inject.Provider;
 import com.jiabangou.ninja.vertx.standalone.ApplicationVertxRoutes;
 import handlers.Chat2Handler;
 import handlers.ChatHandler;
-import handlers.SockJSNinjaAuthHandler;
+import handlers.AuthHandler;
 import io.vertx.core.Vertx;
 import io.vertx.core.eventbus.EventBus;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.handler.sockjs.BridgeOptions;
 import io.vertx.ext.web.handler.sockjs.PermittedOptions;
+import io.vertx.ext.web.handler.sockjs.SockJSHandler;
 
 public class VertxRoutes implements ApplicationVertxRoutes {
 
-    //PS: 所有的Handler都需要用Provider
+    //PS: 所有的Handler都需要用Provider, 否则会有线程安全的问题
     @Inject
     private Provider<ChatHandler> chatHandler;
     @Inject
     private Provider<Chat2Handler> chat2Handler;
     @Inject
-    private Provider<SockJSNinjaAuthHandler> sockJSNinjaAuthHandler;
+    private Provider<AuthHandler> authHandler;
 
     @Override
     public void init(Router router, Vertx vertx) {
 
-        router.route("/eventbus/*").handler(sockJSNinjaAuthHandler.get().bridge(
+        router.route("/eventbus/*").handler(authHandler.get());
+        router.route("/eventbus/*")
+                .handler(SockJSHandler.create(vertx).bridge(
                 new BridgeOptions()
                         .addInboundPermitted(new PermittedOptions().setAddress("chat.to.server"))
                         .addInboundPermitted(new PermittedOptions().setAddress("chat_to_server"))
--- ninja-vertx-sample/src/main/java/handlers/AuthHandler.java
+++ ninja-vertx-sample/src/main/java/handlers/AuthHandler.java
@@ -2,6 +2,7 @@ package handlers;
 
 import com.google.inject.Inject;
 import com.jiabangou.ninja.vertx.standalone.NinjaContextBuilder;
+import io.vertx.core.Handler;
 import io.vertx.core.Vertx;
 import io.vertx.ext.web.RoutingContext;
 import io.vertx.ext.web.handler.sockjs.SockJSHandlerOptions;
@@ -13,27 +14,22 @@ import ninja.session.Session;
  * 带有cookie验证的逻辑
  * Created by freeway on 2016/11/7.
  */
-public class SockJSNinjaAuthHandler extends SockJSHandlerImpl {
+public class AuthHandler implements Handler<RoutingContext> {
 
-
-    private final Vertx vertx;
-
-    private final NinjaContextBuilder ninjaContextBuilder;
 
     @Inject
-    public SockJSNinjaAuthHandler(Vertx vertx, NinjaContextBuilder ninjaContextBuilder) {
-        super(vertx, new SockJSHandlerOptions());
-        this.vertx = vertx;
-        this.ninjaContextBuilder = ninjaContextBuilder;
-    }
+    private Vertx vertx;
+
+    @Inject
+    private NinjaContextBuilder ninjaContextBuilder;
 
     @Override
     public void handle(RoutingContext event) {
         Context context = ninjaContextBuilder.build(event);
         Session session = context.getSession();
-        session.put("hehe", Thread.currentThread().getName());
+        session.put("freeway", Thread.currentThread().getName());
         session.save(context);
-
-        super.handle(event);
+        //TODO: 验证 session 合法性, 如不符合要求就跳转到验证失败的页面
+        event.next();
     }
 }
